add6ce0283f7169dda6ff97b597601c0925f341c
psierrajs <psierrajs@gmail.com>
2024-07-30 18:22:56 +0200

    Defined constants and instance variables for game setup in GamePanel
    - Added screen dimensions (SCREEN_WIDTH, SCREEN_HEIGHT) and unit size
    (UNIT_SIZE).
    - Calculated GAME_UNITS to represent the total number of game units
    based on screen size and unit size.
    - Added DELAY for game update timing.
    - Initialized arrays for storing the x and y coordinates of the snake's
    body parts.
    - Added variables for game state: bodyParts (initial snake length),
    applesEaten (score), appleX and appleY (apple position), direction
    (movement direction), and running (game state flag).
    - Added Timer for game updates and Random for generating random values.

--- Snake/src/main/GamePanel.java
+++ Snake/src/main/GamePanel.java
@@ -5,11 +5,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener{
-
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesEaten;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
 	GamePanel(){
 		
 	}
