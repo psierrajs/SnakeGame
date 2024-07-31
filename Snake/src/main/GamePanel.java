54cbb9f59bea269ce54a80213ceb7884da0d0a25
psierrajs <psierrajs@gmail.com>
2024-07-31 09:29:58 +0200

    Update draw method to handle game running and game over states
    - Add condition to check if the game is running before drawing game
    elements
    - Draw the game grid, apple, and snake only if the game is running
    - Call gameOver method to display game over screen when the game is not
    running
    - Ensure the game panel accurately reflects the current game state

--- Snake/src/main/GamePanel.java
+++ Snake/src/main/GamePanel.java
@@ -53,21 +53,26 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 
 	public void draw(Graphics g) {
-		for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
-			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-			g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-		}
-		g.setColor(Color.red);
-		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-
-		for (int i = 0; i < bodyParts; i++) {
-			if (i == 0) {
-				g.setColor(Color.green);
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			} else {
-				g.setColor(new Color(45, 180, 0));
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+		if (running) {
+			for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+				g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+				g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
 			}
+			g.setColor(Color.red);
+			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+			for (int i = 0; i < bodyParts; i++) {
+				if (i == 0) {
+					g.setColor(Color.green);
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				} else {
+					g.setColor(new Color(45, 180, 0));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+			}
+		}
+		else {
+			gameOver(g);
 		}
 
 	}
@@ -102,7 +107,7 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 
 	public void checkApple() {
-		if((x[0] == appleX) && (y[0] == appleY)) {
+		if ((x[0] == appleX) && (y[0] == appleY)) {
 			bodyParts++;
 			applesEaten++;
 			newApple();
@@ -159,24 +164,24 @@ public class GamePanel extends JPanel implements ActionListener {
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			switch(e.getKeyCode()) {
+			switch (e.getKeyCode()) {
 			case KeyEvent.VK_LEFT:
-				if(direction != 'R') {
+				if (direction != 'R') {
 					direction = 'L';
 				}
 				break;
 			case KeyEvent.VK_RIGHT:
-				if(direction != 'L') {
+				if (direction != 'L') {
 					direction = 'R';
 				}
 				break;
 			case KeyEvent.VK_UP:
-				if(direction != 'D') {
+				if (direction != 'D') {
 					direction = 'U';
 				}
 				break;
 			case KeyEvent.VK_DOWN:
-				if(direction != 'U') {
+				if (direction != 'U') {
 					direction = 'D';
 				}
 				break;
