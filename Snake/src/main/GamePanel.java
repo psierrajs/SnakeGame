683b85be4bf1ffcc8347f908aefc0f0b2f062c0c
psierrajs <psierrajs@gmail.com>
2024-07-31 08:46:31 +0200

    Add paintComponent method to handle custom drawing on the panel
    - Override the paintComponent method to enable custom rendering
    - Call the superclass's paintComponent method to ensure the panel is
    properly rendered
    - Invoke the draw method to render game elements on the panel

--- Snake/src/main/GamePanel.java
+++ Snake/src/main/GamePanel.java
@@ -45,6 +45,8 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
 		
 	}
 	public void draw(Graphics g) {
